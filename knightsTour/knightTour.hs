87f2a305544633c607d29b9ce8c79fc57da21d56
Tycho-Nunnink <ruimtewezen@outlook.com>
2025-05-27 20:54:22 +0200

    knightsTour.hs iets opgeschoond

--- knightsTour/knightTour.hs
+++ knightsTour/knightTour.hs
@@ -5,26 +5,26 @@ type Tour = [Pos]
 
 knightsTour :: Int -> [Tour]
 knightsTour n = foldl (>>=) (return [(1, 1)])
-               $ replicate (n * n - 1)
-               $ moveKnight n
+              $ replicate (n * n - 1)
+              $ moveKnight n
     where moveKnight :: Int -> Tour -> [Tour]
-          moveKnight n path@((a, b):xs) =
-              [newPos:path | newPos@(a', b') <- [(a+1, b+2), (a+1, b-2), (a-1, b+2), (a-1, b-2),
-                                                 (a+2, b+1), (a+2, b-1), (a-2, b+1), (a-2, b-1)],
-                             a' `elem` [1..n]
-                          && b' `elem` [1..n]
+          moveKnight n path@((x, y):xs) =
+              [newPos:path | newPos@(x', y') <- [(x+1, y+2), (x+1, y-2), (x-1, y+2), (x-1, y-2),
+                                                 (x+2, y+1), (x+2, y-1), (x-2, y+1), (x-2, y-1)],
+                             x' `elem` [1..n]
+                          && y' `elem` [1..n]
                           && newPos `notElem` xs] -- één regel korter, joepie!!!!!!
+
               -- do newPos@(a', b') <- [(a+1, b+2), (a+1, b-2), (a-1, b+2), (a-1, b-2),
               --                        (a+2, b+1), (a+2, b-1), (a-2, b+1), (a-2, b-1)]
               --    guard (a' `elem` [1..n] -- "import Control.Monad (guard)" hiervoor nodig
               --        && b' `elem` [1..n]
               --        && newPos `notElem` xs)
-              --    return (newPos:board) -- oud, had besloten het toch met een list comprehension te doen omdat het letterlijk hetzelfde is
+              --    return (newPos:board) -- oud, had besloten het toch met een list comprehension te doen omdat het letterlijk hetzelfde doet en meer leesbaar is
 
 showTour :: Tour -> String
-showTour xs = intercalate " -> " . map show $ reverse xs
+showTour path = intercalate " -> " . map show $ reverse path
 
 main = do
     (n:_) <- getArgs
-    
     mapM (putStrLn . showTour) . knightsTour $ read n 
