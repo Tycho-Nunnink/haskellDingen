cd8ef8cdd4f3bc0c54b5b793d68bd3980fa529b1
Tycho-Nunnink <ruimtewezen@outlook.com>
2025-05-27 23:45:49 +0200

    betterMultiQueens.hs toegevoegd en readme dusdanig aangepast

---
+++ nQueens/betterMultiQueens.hs
@@ -0,0 +1,31 @@
+import System.Environment
+import Data.List (permutations)
+import Control.Monad (guard)
+
+type Board = [Int]
+
+nQueens :: Int -> [Board]
+nQueens n = foldl (>>=) (return []) . replicate n $ addQueens n
+          where addQueens :: Int -> Board -> [Board]
+                addQueens n board = [x:board | x <- [0..n-1], checkBoard 1 x board]
+
+                checkBoard :: Int -> Int -> Board -> Bool
+                checkBoard _ _ [] = True
+                checkBoard offset n (x:xs) = x /= n
+                                          && x /= n - offset
+                                          && x /= n + offset
+                                          && checkBoard (offset + 1) n xs
+
+showQueens :: Int -> Board -> String
+showQueens _ [] = ""
+showQueens size (x:xs) = (concat . replicate x) "- "
+                      ++ "Q "
+                      ++ (concat . replicate (size - x - 1)) "- "
+                      ++ "\n"
+                      ++ showQueens size xs
+
+main = do
+    (arg:_) <- getArgs
+    let n = read arg :: Int
+    mapM (putStrLn . showQueens n) . nQueens $ n
+
--- readme.md
+++ readme.md
@@ -4,4 +4,5 @@
 - polish.hs is een simpele [polish notation](https://en.wikipedia.org/wiki/Polish_notation) calculator
 - nQueens.hs neemt een argument n en lost de [eight queens puzzle](https://en.wikipedia.org/wiki/Eight_queens_puzzle) op voor een n bij n schaakbord
 - multiQueens.hs doet hetzelfde als nQueens.hs maar geeft dan alle opstellingen en doet er aanzienlijk langer over
+- betterMultiQueens.hs doet hetzelfde als multiQueens, maar dan op de snelheid van nQueens, waardoor bijde eigenlijk overbodig zijn geworden
 - KnightsTour.hs neemt een argument n en geeft een pad voor een [knight's tour](https://en.wikipedia.org/wiki/Knight%27s_tour) op een n bij n schaakbord
